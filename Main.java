a81cc6606f85b7882c2d57f7cb174fe75e65ee2e
RubyD <143292762+ohnoruu@users.noreply.github.com>
2024-11-13 19:05:15 +0000

    yay

--- Main.java
+++ Main.java
@@ -4,7 +4,17 @@ public class Main
 {
   public static void main(String[] args)
   {
-    // Put your solutions below
+    // Problem 1
+    String sentence = "gagagigo the giga risen";
+    String target = "gaga";
+    int count = 0;
+    for (int i = 0; i < sentence.length()-(target.length()-1); i++){
+      String L = sentence.substring(i, i+target.length());
+      if (L.equals(target)){
+        count++;
+      }
+    }
+    System.out.println(count);
   }
 
   public static int countSub(String word, String target)
