3b0d931bbb1f3a646602a9266465780013085a62
RubyD <143292762+ohnoruu@users.noreply.github.com>
2024-11-14 19:22:22 +0000

    yay

--- Main.java
+++ Main.java
@@ -4,31 +4,39 @@ public class Main
 {
   public static void main(String[] args)
   {
-    // Problem 1
-    String sentence = "gagagigo the giga risen";
-    String target = "gaga";
-    int count = 0;
-    for (int i = 0; i < sentence.length()-(target.length()-1); i++){
-      String L = sentence.substring(i, i+target.length());
-      if (L.equals(target)){
-        count++;
+    public static int countSub(String word, String target)
+    {
+      int count = 0;
+      for (int i = 0; i < word.length()-(target.length()-1); i++){
+        String L = word.substring(i, i+target.length());
+        if (L.equals(target)){
+          count++;
+        }
       }
+      return count;
     }
-    System.out.println(count);
-  }
 
-  public static int countSub(String word, String target)
-  {
-    return 0;
-  }
+    public static int countProperContains(String word, String target)
+    {
+      int count = 0;
+      int N = target.length();
+      for (int i = 0; i < word.length() - (N-1); i++){
+        String sub = word.substring(i,i+N);
+        boolean beginOrSpaceBefore;
+        int endOfTarget = i + target.length();
+        boolean endOrSpaceAfter = (endOfTarget == word.length() || word.substring(endOfTarget1, endOfTarget+1).equals(" "));
+        boolean properlyContained = beginOrSpaceBefore && endOrSpaceAfter;
+        if (sub.equals(target) && properlyContained){
+          count++;
+        }
+      }
+      return count;
+    }
 
-  public static int countProperContains(String word, String target)
-  {
-    return 0;
-  }
-
-  public static String deleteSub(String word, String target)
-  {
-    return null;
+    public static String deleteSub(String word, String target)
+    {
+      
+      return null;
+    }
   }
 }
